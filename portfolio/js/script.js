97382716946c1e4b78436fd10ec8d75bfb2107d9
AlexanderTheDev <134628095+AlexanderTheDev1@users.noreply.github.com>
2025-01-27 12:15:03 +0200

    removed media="all"
    no need cause its that as a default when not specified.

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -3,3 +3,15 @@ let sentence = "JavaScript is awesome!";
 let position = sentence.indexOf("awesome");
 
 console.log(position);
+
+let age = prompt("How old are you");
+if (age != null) {
+    console.log(`You are ${age} years old.`);
+} else {
+    console.log("User cancelled the prompt");
+}
+do {
+    console.log("Put Your age");
+    age = prompt("How old are you");
+} while (age = null);
+ 
