a1a5130bf011544cd9420565351536a4967d4ba7
AlexanderTheDev <134628095+AlexanderTheDev1@users.noreply.github.com>
2025-01-28 13:41:30 +0200

    js button rotate

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,7 +1,6 @@
 document.getElementById("menu-toggle").addEventListener("click", function() {
     console.log("Button clicked");
     document.querySelector(".menu-wrap").classList.toggle('menu-active');
-    document.querySelector(".ham-menu").classList.toggle('ham-menu2');
-});
-
- 
+    document.querySelector("#ham-menu").classList.toggle('ham-menu2');
+    this.classList.toggle('menu-rotated');
+}); 
