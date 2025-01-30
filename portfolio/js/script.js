e3a619d7d92421bcb9c9f5c2827b380703bc10f2
AlexanderTheDev <134628095+AlexanderTheDev1@users.noreply.github.com>
2025-01-30 11:03:48 +0200

    js dark mode

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -4,3 +4,18 @@ document.getElementById("menu-toggle").addEventListener("click", function() {
     document.querySelector("#ham-menu").classList.toggle('ham-menu2');
     this.classList.toggle('menu-rotated');
 }); 
+const toggle = document.querySelector(".button-change");
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+    document.documentElement.setAttribute('data-theme', savedTheme);
+}
+toggle.addEventListener('click', () => {
+    let currentTheme =  document.documentElement.getAttribute('data-theme');
+    if (currentTheme === 'light') {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}); 
---
+++ projects/technical-documentation-page/script.js
@@ -0,0 +1,15 @@
+const toggle = document.querySelector(".button-change");
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+    document.documentElement.setAttribute('data-theme', savedTheme);
+}
+toggle.addEventListener('click', () => {
+    let currentTheme =  document.documentElement.getAttribute('data-theme');
+    if (currentTheme === 'light') {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+});
